c985e1c4ca73da29331fabe312e0bf5e81a04980
Benjamin Zagorski <benj@zagorski.com>
2025-09-09 06:24:28 -0500

    Added field validations and greyed out loaners that are not calculated yet

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,24 +1,61 @@
+import { useState } from 'react';
 import './Inputs.css';
 
 const Inputs = ({onCreditScoreChange, onLoanAmountChange}) => {
+    const [creditScoreFieldInvalid, setCreditScoreFieldInvalid] = useState(false)
+    const [loanAmountFieldInvalid, setLoanAmountFieldInvalid] = useState(false)
+
+    const validateCreditScore = value => {
+        if (value < 300 || value > 850) {
+            setCreditScoreFieldInvalid(true)
+            return false
+        } else {
+            return true
+        }
+    }
+
+    const creditScoreChange = (value) => {
+        if(validateCreditScore(value)) {
+            setCreditScoreFieldInvalid(false)
+        }
+        onCreditScoreChange(value)
+    }
+
+    const validateLoanAmount = value => {
+        if (!value) {
+            setLoanAmountFieldInvalid(true)
+            return false
+        } else {
+            return true
+        }
+    }
+
+    const loanAmountChange = value => {
+        if(validateLoanAmount(value)) {
+            setLoanAmountFieldInvalid(false)
+        }
+        onLoanAmountChange(value)
+    }
 
     return (
         <div className="inputs">
             <a className={"credit-score input-text"}>Credit Score:</a>
             <input
-                className="credit-score-input-field input-field"
-                onChange={(event) => onCreditScoreChange(event.target.value)}
+                className={`credit-score-input-field input-field ${creditScoreFieldInvalid && "invalid"}`}
+                onChange={(event) => creditScoreChange(event.target.value)}
                 type="number"
                 onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}
+                onBlur={(event) => validateCreditScore(event.target.value)}
             />
             <a className={"loan-amount input-text"}>Loan Amount:</a>
             <div>
                 <a className="dollar-sign-display">$</a>
                 <input
-                    className="loan-amount-input-field input-field"
-                    onChange={(event) => onLoanAmountChange(event.target.value)}
+                    className={`loan-amount-input-field input-field ${loanAmountFieldInvalid && "invalid"}`}
+                    onChange={(event) => loanAmountChange(event.target.value)}
                     type="number"
                     onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}
+                    onBlur={(event) => validateLoanAmount(event.target.value)}
                 />
             </div>
         </div>
--- src/LenderDisplay.js
+++ src/LenderDisplay.js
@@ -1,15 +1,15 @@
 import "./LenderDisplay.css"
 
-const LenderDisplay = ({name, interestRates=[0,0,0,0,0], monthlyPayments=[0,0,0,0,0]}) => {
+const LenderDisplay = ({name, faded, interestRates=[0,0,0,0,0], monthlyPayments=[0,0,0,0,0]}) => {
     return (
         <div className="lender-display">
             <h1>{name}</h1>
             {[5, 7, 10, 12, 15].map((loanLength, index) => {
                 return(
-                    <div>
-                        <h1>{loanLength} Year</h1>
-                        <h3>Interest Rate: {interestRates[index] ? interestRates[index] : "∅"}%</h3>
-                        <h3>Monthly Payment: ${monthlyPayments[index] ? monthlyPayments[index].toFixed(2) : "∅"}</h3>
+                    <div key={index}>
+                        <h1 className={faded && 'faded'}>{loanLength} Year</h1>
+                        <h3 className={faded && 'faded'}>Interest Rate: {interestRates[index] ? interestRates[index] : "∅"}%</h3>
+                        <h3 className={faded && 'faded'}>Monthly Payment: ${monthlyPayments[index] ? monthlyPayments[index].toFixed(2) : "∅"}</h3>
                     </div>
                 )
             })}
