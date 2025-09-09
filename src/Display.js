19e51eec410d905d5a7980a318b4ea8bba168e1b
Benjamin Zagorski <benj@zagorski.com>
2025-09-09 04:46:10 -0500

    Now calculates monthly payments for Lender A

--- src/Display.js
+++ src/Display.js
@@ -1,9 +1,38 @@
 import './Display.css'
 import LenderDisplay from './LenderDisplay';
+const YEARS = [5, 7, 10, 12, 15]
 const Display = ({loanAmount, creditScore}) => {
+
+    const calculateLenderAInterests = () => {
+        if (creditScore > 300 && creditScore < 450) {
+            return Array(5).fill(17.99)
+        } else if (creditScore >= 450 && creditScore < 600) {
+            return Array(5).fill(16.99)
+        } else if (creditScore >= 600 && creditScore < 700) {
+            return Array(5).fill(14.49)
+        } else if (creditScore >= 700 && creditScore < 800) {
+            return Array(5).fill(12.49)
+        } else if (creditScore >= 800 && creditScore < 850) {
+            return Array(5).fill(10.99)
+        } else { return Array(5).fill(null) }
+    }
+
+    const calculateMonthlyPayments = (interests) => {
+        return YEARS.map((numYears, index) => {
+            const monthlyInterestRate = interests[index]/1200.0
+            const numberMonthlyPayments = numYears * 12
+            const onePlusR = 1 + monthlyInterestRate
+            const onePlusRToTheN = onePlusR ** numberMonthlyPayments
+            const numerator = loanAmount * monthlyInterestRate * onePlusRToTheN
+            const denominator = onePlusRToTheN - 1.0
+            return numerator / denominator
+        })
+    }
+
     return (
         <div className="display">
-            {["A", "B", "C", "D", "E"].map(name => {
+            <LenderDisplay name="Lender A" interestRates={calculateLenderAInterests()} monthlyPayments={calculateMonthlyPayments(calculateLenderAInterests())}/>
+            {["B", "C", "D", "E"].map(name => {
                 return <LenderDisplay name={`Lender ${name}`}></LenderDisplay>
             })}
         </div>
--- src/LenderDisplay.js
+++ src/LenderDisplay.js
@@ -1,15 +1,15 @@
 import "./LenderDisplay.css"
 
-const LenderDisplay = ({name}) => {
+const LenderDisplay = ({name, interestRates=[0,0,0,0,0], monthlyPayments=[0,0,0,0,0]}) => {
     return (
         <div className="lender-display">
             <h1>{name}</h1>
-            {[5, 7, 10, 12, 15].map(loanLength => {
+            {[5, 7, 10, 12, 15].map((loanLength, index) => {
                 return(
                     <div>
                         <h1>{loanLength} Year</h1>
-                        <h3>Interest Rate: 5.99%</h3>
-                        <h3>Monthly Payment: $593.27</h3>
+                        <h3>Interest Rate: {interestRates[index] ? interestRates[index] : "∅"}%</h3>
+                        <h3>Monthly Payment: ${monthlyPayments[index] ? monthlyPayments[index].toFixed(2) : "∅"}</h3>
                     </div>
                 )
             })}
